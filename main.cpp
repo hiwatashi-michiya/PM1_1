508c9b72df4e358f149252e29b14db18444cea44
樋渡充也 <@MSI>
2022-10-05 11:52:30 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+	printf("Hello World!\n");
+	
+	printf("コンソールアプリケーションを終了する場合は数字の1を押してEnterキーを入力してください");
+
+	int i = 0;
+	while (i != 1) {
+		scanf_s("%d", &i);
+	}
+
+	return 0;
+}
